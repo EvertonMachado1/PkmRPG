f2253d3267eade09195ee70a68d59a9d459b4bfe
EvertonMachado1 <106996058+EvertonMachado1@users.noreply.github.com>
2025-05-12 20:03:22 +0000

    criados as pastas de template

--- PkmRPG/PkmRPG/urls.py
+++ PkmRPG/PkmRPG/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('play/', include('play.urls')),
+    path('menage/', include('menage.urls')),
 ]
