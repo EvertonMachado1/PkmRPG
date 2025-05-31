f8b2d6193a07e7c695fe935e450876a30a90d346
EvertonMachado1 <everton1234ramosmachado@gmail.com>
2025-05-31 16:53:40 -0300

    criado editarPkm.js

--- dangerousArea/public/admin/editarPkm.js
+++ dangerousArea/public/admin/editarPkm.js
@@ -43,6 +43,56 @@ async function editarPokemon(id) {
 }
 
 async function salvarEdicao() {
-    const id = window.pokemonEditando
-    
+    const id = window.pokemonEditando;
+    if (!id) return;
+    const nome = document.getElementById("nomePoke").value.trim();
+    const numero_pokedex = parseInt(document.getElementById("numPoke").value);
+    const tipo1 = document.getElementById("tipo1").value;
+    const tipo2 = document.getElementById("tipo2").value || null;
+    const evolucao = document.getElementById("evolucao").value.trim() || "N/A";
+    const vida = parseInt(document.getElementById("vidaPoke").value) || 0;
+    const ataque = parseInt(document.getElementById("atk").value) || 0;
+    const defesa = parseInt(document.getElementById("def").value) || 0;
+    const ataque_especial = parseInt(document.getElementById("atk_esp").value) || 0;
+    const defesa_especial = parseInt(document.getElementById("def_esp").value) || 0;
+    const velocidade = parseInt(document.getElementById("vel").value) || 0;
+    const descricao = document.getElementById("descricao").value.trim() || "N/A";
+
+    if (!nome || isNaN(numero_pokedex)) {
+    mostrarToast("❌ Nome e Nº Pokédex são obrigatórios.");
+    return;
+  }
+    const dados = {
+        nome,
+        numero_pokedex,
+        tipo1,
+        tipo2,
+        evolucao,
+        vida,
+        ataque,
+        defesa,
+        ataque_especial,
+        defesa_especial,
+        velocidade,
+        descricao
+    };
+
+    try {
+        const res = await fetch(`/api/pokemons/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(dados)
+        });
+
+        if (res.ok) {
+        mostrarToast("✅ Pokémon atualizado com sucesso!");
+        setTimeout(() => {
+            fecharFormulario();
+        }, 2000);
+        } else {
+        mostrarToast("❌ Erro ao atualizar Pokémon.");
+        }
+    } catch (err) {
+        mostrarToast("❌ Erro na comunicação com o servidor.");
+    }
 }
